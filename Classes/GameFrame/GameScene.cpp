06b687d9963504308c2d0924541dc589cc83b1ca
404wrong <2497440459@qq.com>
2024-01-22 11:10:41 +0800

    Update GameScene.cpp

--- Classes/GameFrame/GameScene.cpp
+++ Classes/GameFrame/GameScene.cpp
@@ -113,7 +113,7 @@ bool GameScene::init()
     optMenu->alignItemsHorizontallyWithPadding(100);
     optMenu->setPosition(Vec2(winSize.width / 2, 48));
     this->addChild(optMenu, 1);
-    this->schedule(CC_SCHEDULE_SELECTOR(GameScene::getmove), 1.0f);
+    this->schedule(CC_SCHEDULE_SELECTOR(GameScene::getmove), 0.1f);
 
     SaveInfo* info = SaveManager::getInstance()->info;
     if (info) {
